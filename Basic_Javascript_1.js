72bf533b7be82b234354150e9fef1d71f0df4887
mohammad6182 <104463077+mohammad6182@users.noreply.github.com>
2022-05-04 13:54:24 -0700

    add

--- Basic_Javascript_1.js
+++ Basic_Javascript_1.js
@@ -1,1 +1,8 @@
 alert("Hello, welcome to my website!!");
+
+<script>
+	function changeColor(newColor) {
+	  var elem = document.getElementById('persian');
+	  elem,style.color = newColor;
+}
+</script>
